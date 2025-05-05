c17d59ee5f2048b316f31892f35621fb1a275fef
Nakul Chouksey <71.nakul@gmail.com>
2025-05-05 23:29:20 +0200

    add: landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,26 @@
 import Bg from "@/components/ui/background";
+import { HoverBorderGradient } from "@/components/ui/hover-border-gradient";
 import React from "react";
+import Link from "next/link";
 
 const page = () => {
   return (
     <div className="relative w-full h-screen flex items-center justify-center flex-col px-4">
       <Bg />
-      <h2 className="bg-clip-text text-transparent text-center bg-gradient-to-b from-neutral-900 to-neutral-700 dark:from-neutral-600 dark:to-white text-2xl md:text-4xl lg:text-7xl font-sans py-2 md:py-10 relative z-20 font-bold tracking-tight">
+      <h2 className="bg-clip-text text-transparent text-center bg-gradient-to-b from-neutral-900 to-neutral-700 dark:from-neutral-600 dark:to-white text-4xl md:text-7xl lg:text-9xl font-sans relative z-20 font-bold tracking-tight">
         Mindrift
       </h2>
-      <p className="max-w-xl mx-auto text-sm md:text-lg text-neutral-700 dark:text-neutral-400 text-center">
-        Get the best advices from our experts, including expert artists,
-        painters, marathon enthusiasts and RDX, totally free.
+      <p className="max-w-xl mx-auto text-sm md:text-lg z-20 text-neutral-400 text-center">
+        Put $1 in your IQ.
       </p>
+      <Link href={"/"}></Link>
+      <HoverBorderGradient
+        containerClassName="rounded-full border-zinc-950 cursor-pointer mt-9"
+        as="button"
+        className="dark:bg-black dark:text-white flex items-center space-x-2"
+      >
+        <span>Connect your wallet</span>
+      </HoverBorderGradient>
     </div>
   );
 };
